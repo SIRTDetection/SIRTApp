4d069bfb1335e2e9abbb7518e2a6f2d4283d954b
Robertoscuro <rober-alga@outlook.com>
2018-12-04 20:26:54 +0100

    Camera working

--- app/src/main/java/sirtapp/sirtdetection/com/sirtapp/MainActivity.java
+++ app/src/main/java/sirtapp/sirtdetection/com/sirtapp/MainActivity.java
@@ -1,7 +1,13 @@
 package sirtapp.sirtdetection.com.sirtapp;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,15 +15,34 @@ public class MainActivity extends AppCompatActivity {
     static {
         System.loadLibrary("native-lib");
     }
-
+    ImageView image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    Button button=(Button)findViewById(R.id.button_capture);
+    image =(ImageView)findViewById(R.id.imageView);
 
+
+
+    button.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(intent,0);
+
+        }
+    });
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap=(Bitmap)data.getExtras().get("data");
+        image.setImageBitmap(bitmap);
+    }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
