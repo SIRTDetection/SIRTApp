17433a69e57a0db61beb0572e9d5aa95020167e5
Robertoscuro <rober-alga@outlook.com>
2018-12-26 11:46:40 +0100

    interface fixed

--- app/src/main/java/sirtapp/sirtdetection/com/sirtapp/MainActivity.java
+++ app/src/main/java/sirtapp/sirtdetection/com/sirtapp/MainActivity.java
@@ -5,23 +5,17 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.media.Image;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.OutputStream;
-import java.util.Random;
 
 import static android.app.PendingIntent.getActivity;
 
@@ -38,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    Button button=(Button)findViewById(R.id.button2);
+    Button button=(Button)findViewById(R.id.camera);
     image =(ImageView)findViewById(R.id.image);
 
 
